220f6b1a1803c9388aba58db19f5c6fb79aef625
Jaron <jaron@server.fake>
2013-04-23 09:46:21 +0100

    initial commit

---
+++ src/main/r/analyse.R
@@ -0,0 +1,73 @@
+library(igraph)
+
+# change this to your working directory (or where you want the graphs downloaded)
+setwd("/users/jaron/Documents/workspace/railgraph/graphs")
+
+fileName = "tubeDLR.graphml"
+if (file.exists(fileName)) {
+  print("Graph file already exists")
+} else {
+  print("Downloading graph file")
+  dest =  paste("https://github.com/jaron/railgraph/blob/master/graphs/", fileName, sep="") 
+  download.file(dest, destfile=fileName, method="curl")
+}
+g = read.graph(fileName, format="graphml")
+
+# obtain summary information about the graph
+summary(g)
+
+layoutFR <- layout.fruchterman.reingold(g, niter=600, area=vcount(g)^1.9, repulserad=vcount(g)^3)
+plot(g, layout=layoutFR, vertex.size=V(g)$degree, vertex.label=NA)
+
+# degree.distribution should give you the number of nodes with each specific edge count
+range(degree(g, mode="total"))
+degree.distribution(g, T)
+
+# calculate the in and out degrees separately
+# use the degree() function and options for calculating directed degree
+# see the documentation here: http://igraph.sourceforge.net/documentation.html
+degreeVal = degree(g)
+
+# see which node has the max degree
+V(g)$label[which.max(degreeVal)]
+V(g)$label[which(degreeVal == 7)]
+V(g)$label[which(degreeVal == 6)]
+
+
+# find undirected betweenness (betweenness()) scores and then nodes with the max betweenness
+# warning, can be slow with large graphs, you may consider betweenness.estimate instead
+
+bb = betweenness(g)
+V(g)$label[which.max(bb)]
+# show neighbours
+V(g)$label[V(g)[nei(which.max(bb))]]
+
+
+# Leverage centrality calculation taken from http://igraph.wikidot.com/r-recipes#toc10
+lvcent <- function(graph){
+  k <- degree(graph)
+  n <- vcount(graph)
+  sapply(1:n, function(v) { mean((k[v]-k[neighbors(graph,v)]) / (k[v]+k[neighbors(graph,v)])) })
+}
+lv = lvcent(g)
+
+# merge data values into a single data frame
+df <- data.frame(V(g)$label, bb, degreeVal, lv)
+col.names=c("Station", "Betweenness", "Degree", "Leverage")
+names(df) <- col.names
+
+df[ order(-df[,2], df[,1]), ]
+
+
+# calculate Page Rank (page.rank()) and find the node having the highest pagerank
+# you'll want the $vector portion of the answer returned
+
+pr = page.rank(g)
+V(g)$label[which.max(pr$vector)]
+V(g)$label[V(g)[nei(which.max(pr$vector))]]
+
+
+
+
+
+
