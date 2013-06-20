b3e93650ff3839a6b7003a1291e0a44983017e24
Jaron <jaron@server.fake>
2013-06-20 10:21:39 +0100

    removed unused imports

--- src/main/java/org/memento/railgraph/App.java
+++ src/main/java/org/memento/railgraph/App.java
@@ -2,12 +2,10 @@ package org.memento.railgraph;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Iterator;
 
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.graphdb.index.Index;
 import org.neo4j.kernel.GraphDatabaseAPI;
